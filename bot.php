62aa75b1b403ac4aaff0eaab6832d62f7cf18b99
Anat Abdullagasim <anat.pantera@gmail.com>
2017-09-15 17:23:04 +0700

    Modified reply message

--- bot.php
+++ bot.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 
       // Build message to reply back
       if (strpos($text, 'วันที่') !== false) {
-          $text = date('d/m/Y').' ไง ปฏิทินไม่มีหรอ (unamused)';
+          $text = date('d/m/Y').' ไง ปฏิทินไม่มีหรอ...';
       }
       
       $messages = [
